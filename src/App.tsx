de7f45f0660e95ce53ec984e38a039156e7c1923
Rafael Aguilar Alvear <rafael.aguilara@softtekprojects.com>
2025-02-24 13:03:59 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import './App.css'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [pokemonData, setPokemonData] = useState<{name: string, base_experience: string} | null >(null);
+  const[pokemonImage, setPokemonImage] = useState<string>('');
+  const [error, setError] = useState<string>('');
+
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
+      .then(response => {
+        setPokemonData(response.data);
+        setPokemonImage(response.data.sprites.front_shiny);
+        console.log(response.data);
+      })
+  
+      .catch(() => {
+        setError('Pokemon not found');
+      });
+  }, []); 
+  
+  return (
+    <>
+      <h1>Pokemon</h1>
+
+      { pokemonData &&
+        <>
+          <h2>{pokemonData.name}</h2> 
+          <img src={pokemonImage} alt="" />
+          <p>{pokemonData.base_experience}</p>
+       </>
+      }
+      
+      {error && <p>{error}</p>}
+    </>
+  );
+}
+
+export default App
+
+
